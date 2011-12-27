04bfa8f880c5daac6cb9a8f2efd6b008f1098275
Vishal Kanaujia <vishalkanaujia@gmail.com>
2011-12-27 16:21:48 +0530

    first commit

---
+++ debug.py
@@ -0,0 +1,30 @@
+import sys
+import tokenize
+
+class pydbg:
+    def __init__(self, modname=None):
+        self.level = 0
+        self.modname = modname
+
+    def DLOG(self, lvl=None, fmt=None, *args):
+        self.level = lvl
+
+        if 0 == self.level:
+            return
+        print '%s ' %self.modname
+      
+        fmtlist = fmt.split()
+
+        #Disabling new line of print python
+        sys.stdout.write("%s:" %(self.modname))
+
+        i = 0
+        for arg in args:
+            sys.stdout.write(fmtlist[i] %(arg))
+            sys.stdout.write(" ")
+            i = i + 1
+
+        print ""
+
+    def DLOG2(self, lvl=None, fmt=None, **args):
+        self.level = lvl
---
+++ example.py
@@ -0,0 +1,22 @@
+import trie
+
+# Create the tree
+mytrie = trie.Trie()
+
+# Get the input ready
+s = "The quick brown fox jumps over the lazy dog"
+
+toks = s.strip().split()
+
+for tok in toks:
+     # Insert key:value {word: meaning} in to tree
+     mytrie.addWord(mytrie.rootNode, tok, "blabla")
+
+# If search is successful, you will get meaning in
+# a list
+result = False
+meaning = []
+result = mytrie.search(mytrie.rootNode, "lazy", meaning)
+
+# Output
+print meaning
